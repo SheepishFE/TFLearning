496c7d0bf13eaf8dd5521a288c0b8498a6db1878
SheepsFE <ojcc1992@gmail.com>
2022-08-22 12:53:47 +0100

    Update vars.tf

--- ebstf/vars.tf
+++ ebstf/vars.tf
@@ -34,13 +34,3 @@ variable "management_vpc_cidr" {
 //variable "subnet_id" {
  // description = "public subnet ID"
 //}
-
-
-locals {
-
-  # NEW MANAGEMENT VPC
-  mgmt_vpc_id        = "vpc-0b8be048102bf2a4f"
-  mgmt_subnet_aza_id = "subnet-034c71ab50ce029fd"
-
-}
-
